5d4b749218b37ecd1db8fb5a10072ac40bd82524
Matthew Nevelos <matthewnevelos@gmail.com>
2026-04-17 17:16:15 -0600

    clean up main

--- main.py
+++ main.py
@@ -4,26 +4,23 @@ from src.models import *
 from src.plot import *
 from src.train import train_model
 from src.transcribe import transcribe_audio
-from src.format_converter import midi_to_sheet, audio_to_CQT, audio_to_STFT
-from src.evaluate import eval_metrics
 from src.export_onnx import export_onnx
 import torch
 from torch.utils.data import DataLoader
-from pathlib import Path
             
 if __name__ == "__main__":
     
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     preprocessor = MaestroPreprocessor(hop_length=128).to(device)
 
-    #defines the path of csv and raw data
+    # defines the path of csv and raw data
     maestro_path = r"D:/databases/maestro-v3.0.0"
-    # maestro_path = r"Example Data/MAESTRO"
-    # maestro_path = r"T:/Uni/Final year/Digital Winter Sem/Final Project/maestro-v3.0.0/maestro-v3.0.0"
-
-    preprocessor.precompute_full_dataset(maestro_path)
     metadata = load_metadata(maestro_path)
 
+    #precompute audio to Pytorch tensors
+    preprocessor.precompute_full_dataset(maestro_path)
+
+    # set up datasets
     train_dataset = MaestroDataset(
         maestro_dir=maestro_path, 
         target_sr=preprocessor.target_sr, 
@@ -47,7 +44,8 @@ if __name__ == "__main__":
         metadata=metadata["test"],
         segment_seconds=5,
         return_onsets=True)
-
+    
+    #Set up data loaders
     train_dataloader = DataLoader(
         train_dataset,
         batch_size=24,
@@ -74,25 +72,28 @@ if __name__ == "__main__":
 
     model = PianoOnsetFrameCRNN().to(device)
 
-    # model = train_model(
-    #     model,
-    #     preprocessor, 
-    #     train_dataloader,
-    #     valid_dataloader,
-    #     epochs=3,
-    #     augment=False)
+    #training loop
+    model = train_model(
+        model,
+        preprocessor, 
+        train_dataloader,
+        valid_dataloader,
+        epochs=3,
+        augment=False)
     
-    state_dict = torch.load("trained_models/onsets_frames_epoch_4.pth", map_location=device, weights_only=True)
+    #load model if just evaluating
+    state_dict = torch.load("trained_models/onsets_frames_epoch_5.pth", map_location=device, weights_only=True)
     model.load_state_dict(state_dict)
     
-    # AUDIO_FILE = "test_set/sheep/Sheep.wav"
-    # OUTPUT_FOLDER = "test_set/sheep"
-    # # audio, _ = torchaudio.load(AUDIO_FILE)
-    # # plot_CQT(preprocessor(audio, augment=False, orig_sr=preprocessor.target_sr), show=True)
-    # sheet = transcribe_audio(AUDIO_FILE, model, OUTPUT_FOLDER, onset_threshold=0.3,)      
-
+    #convert to onnx model for interactive GitHub Pages app
     export_onnx(model, "docs/songscribe.onnx")
     
-    # AUDIO_FILE = "test_set/signal flags/Signal Flags.wav"
-    # OUTPUT_FOLDER = "test_set/signal flags"
-    # sheet = transcribe_audio(AUDIO_FILE, model, OUTPUT_FOLDER, show=True)
+    #transcribe baa baa black sheep
+    AUDIO_FILE = "test_set/sheep/Sheep.wav"
+    OUTPUT_FOLDER = "test_set/sheep"
+    sheet = transcribe_audio(AUDIO_FILE, model, OUTPUT_FOLDER, onset_threshold=0.3,)      
+
+    # transcribe signal flags
+    AUDIO_FILE = "test_set/signal flags/Signal Flags.wav"
+    OUTPUT_FOLDER = "test_set/signal flags"
+    sheet = transcribe_audio(AUDIO_FILE, model, OUTPUT_FOLDER, show=True)
